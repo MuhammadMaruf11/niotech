db5031d66b396542963fae8e2ed6752845e50d25
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-09-15 18:38:58 +0600

    product page

--- src/components/OurProducts/AllProducts.tsx
+++ src/components/OurProducts/AllProducts.tsx
@@ -14,13 +14,13 @@ interface AllProductsProps {
 
 const AllProducts: FC<AllProductsProps> = ({ categories }) => {
     return (
-        <section className="pt-12">
+        <section className="py-12">
             <div className="grid lg:grid-cols-3 md:grid-cols-2 xl:gap-16 lg:gap-6 gap-6 px-3 max-w-screen-xl mx-auto">
                 {categories?.map((data, index) => (
-                    <div key={index} className="bg-white xl:py-14 py-8 xl:px-6 px-4">
-                        <h3>{data.CategoryName}</h3>
-                        <Image src={`https://theniotech.com/assets/images/CategoryImages/${data.ImagePath}`} width={200} height={50} alt="product" />
-                        <Link href={`/products/${data.CategoryID}`}>View Details</Link>
+                    <div key={index} className="bg-white p-5 border border-gray-300 text-center h-96 flex flex-col justify-between">
+                        <h3 className="font-normal text-xl text-[#333]">{data.CategoryName}</h3>
+                        <Image src={`https://theniotech.com/assets/images/CategoryImages/${data.ImagePath}`} width={334} height={222} alt="product" />
+                        <Link className="mt-3 border border-black text-[#242424] w-100 inline-flex h-12 justify-center items-center hover:text-white hover:bg-black" href={`/products/${data.CategoryID}`}>View Details</Link>
                     </div>
                 ))}
             </div>
